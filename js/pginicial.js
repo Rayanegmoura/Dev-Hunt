977bc6eb0f586b4b5cb6eb87634fbdf61195fe5b
rayanegmoura <rayanegomes_m@hotmail.com>
2024-09-12 15:12:31 -0300

    atualizção

--- js/pginicial.js
+++ js/pginicial.js
@@ -1,3 +1,17 @@
+// ACESSIBILIDADE MODO CLARO
+
+let trilho = document.getElementById('trilho')
+let body = document.querySelector('body')
+
+trilho.addEventListener('click', ()=>{
+    trilho.classList.toggle('dark')
+    body.classList.toggle('dark')
+})
+
+
+
+
+// SCROLLREVEAL
 
 window.revelar = ScrollReveal({reset:true})
 

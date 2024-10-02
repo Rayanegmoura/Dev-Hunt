673005a22c0c7abec335758f0fce152dac81720b
RafaelMDev29 <l.rafael29@outlook.com>
2024-10-02 11:11:44 -0300

    Update userempresa.js

--- js/userempresa.js
+++ js/userempresa.js
@@ -1,1 +1,12 @@
+let lastScrollTop = 0;
+    const header = document.querySelector('header');
 
+    window.addEventListener('scroll', function() {
+        let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+        if (scrollTop > lastScrollTop) {
+            header.style.top = "-100px";
+        } else {
+            header.style.top = "0";
+        }
+        lastScrollTop = scrollTop;
+    });

27b8eded0939d811382083ec102273ca5fa9e7fc
ProgramadoRyan <ryanrodrygs1@gmail.com>
2024-10-11 08:56:06 -0300

    atl.

--- js/pagina-admin.js
+++ js/pagina-admin.js
@@ -93,5 +93,37 @@ const data1 = {
     });
   });
   
+  // Gráfico de rosca para 'Interação' no Tube
+  const data2 = {
+    labels: [
+      'Red',
+      'Green',
+      'Yellow',
+      'Grey',
+      'Blue'
+    ],
+    datasets: [{
+      label: 'My First Dataset',
+      data: [11, 16, 7, 3, 14],
+      backgroundColor: [
+        'rgb(255, 99, 132)',
+        'rgb(75, 192, 192)',
+        'rgb(255, 205, 86)',
+        'rgb(201, 203, 207)',
+        'rgb(54, 162, 235)'
+      ]
+    }]
+  };
   
+  const config3= {
+    type: 'polarArea',
+  data: data2,
+  options: {}
+};
+  
+  
+  const chartTube = new Chart(
+    document.getElementById('tubeChart'),
+    config3
+  );
   
